9c9509fd300355a2c8df5c4f19c241b07c3b729b
JP2706 <jpelczar0327@gmail.com>
2023-12-12 19:15:05 +0100

    Dodaj pliki projektów.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class FizzBuzz
+    {
+        public string UserChoice(int userNum)
+        {
+            if (userNum % 3 == 0 && userNum % 5 == 0)
+                return "FizzBuzz";
+
+
+            if (userNum % 3 == 0)
+                return "Fizz";
+
+
+            if (userNum % 5 == 0)
+                return "Buzz";
+
+
+            return userNum.ToString();
+
+
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gra FizzBuzz");
+            var fizzBuzz = new FizzBuzz();
+            while (true)
+            {
+                Console.Write("Podaj liczbe:");
+                var userNum = GetNum();
+                Console.WriteLine(fizzBuzz.UserChoice(userNum));
+                Console.WriteLine("Chcesz sprawdzić kolejną liczbę?(Jeśli nie to wpisz nie)");
+                if (Console.ReadLine() == "nie")
+                    break;
+            }
+        }
+
+        private static int GetNum()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(), out int userNum))
+                {
+                    Console.Write("Błędne dane\nPodaj właściwe dane:");
+                    continue;
+
+                }
+
+                return userNum;
+            }
+        }
+    }
+}
